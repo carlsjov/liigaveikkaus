07f29b696272b3683446f12a8cb8aa834e19e5ec
JohanS <joalsj@utu.fi>
2017-12-05 22:05:15 +0200

    TeamsGuess luotu joka perii luokan Teams ominaisuudet. Estää joukkueen paikon vaihtamisen, kehnolla tavalla mutta toimii silti.

---
+++ TeamsGuess.java
@@ -0,0 +1,17 @@
+/*
+TeamsGuess on arvaajan joukkue olio. Olio perii team olion ominaisuuksia Teams
+*/
+public class TeamsGuess extends Teams{
+    int points;
+    public TeamsGuess(String name,int place){
+        super(name);
+        this.place = place;
+    }
+    private void setPoints(int points) {
+        this.points = points;
+    }
+    private void setPlace(int place) {System.out.println("No posible");}
+    private int getPoints() {
+        return points;
+    }
+}
--- teams.java
+++ teams.java
@@ -4,16 +4,14 @@ Vaatii luodessa nimen string muodossa (Nimi voidaan asettaa vain kerran)
 */
 import java.io.*;
 public class Teams {
-final String name;
-int place;
-public Teams(String name) {this.name = name;}
-//Asetetaan int place
-private void setPlace(int place) {this.place = place;}
-//Annetaan name ja place
-private String giveName() {return name;}
-private int givePlace() {return place;}
-//Tulostus
-public String toString() {
-    return "Team name: "+name+" Place: "+place;
-   }
+    final String name;
+    int place;
+    public Teams(String name) {this.name = name;}
+    //Asetetaan int place
+    private void setPlace(int place) {this.place = place;}
+    //Annetaan name ja place
+    private String giveName() {return name;}
+    private int givePlace() {return place;}
+    //Tulostus
+    public String toString() {return "Team name: "+name+" Place: "+place;}
 }
