691c52fbf23404cb9ea47b5901b3af0fa35d8e0d
JohanS <joalsj@utu.fi>
2017-12-05 20:52:06 +0200

    Liiga joukkueet saadaan nyt tehtyä taulukkoon liiga[] jonka olioita on Teams luokka.

---
+++ liigajoukkueet.java
@@ -0,0 +1,21 @@
+public class liigajoukkueet {
+    public static void main(String args[]){
+        Teams[] liiga = new Teams[15];
+        liiga[0] = new Teams("Ässät");
+        liiga[1] = new Teams("Lukko");
+        liiga[2] = new Teams("TPS");
+        liiga[3] = new Teams("Kärpät");
+        liiga[4] = new Teams("HPK");
+        liiga[5] = new Teams("JYP");
+        liiga[6] = new Teams("HIFK");
+        liiga[7] = new Teams("Sport");
+        liiga[8] = new Teams("Jukurit");
+        liiga[9] = new Teams("KooKoo");
+        liiga[10] = new Teams("SaiPa");
+        liiga[11] = new Teams("Pelicans");
+        liiga[12] = new Teams("KalPa");
+        liiga[13] = new Teams("Tappara");
+        liiga[14] = new Teams("Ilves");
+        System.out.println("joukkueet tehty");
+    }
+}
--- teams.java
+++ teams.java
@@ -1,30 +1,9 @@
 import java.io.*;
-public class teams {
-String name;
+public class Teams {
+final String name;
 int place;
-int guess;
 
-public teams(String name) {
+public Teams(String name) {
     this.name = name;
-}
-public void teamsPlace(int teamsPlace) {
-    teamsPlace = place;
-}
-public void teamsGuess(int teamsGuess){
-    teamsGuess = guess;
-}
-public void setGuess(int guess) {
-    try {
-        cin = new InputStreamReader(System.in);
-        System.out.println("Enter your guess for "+ name +":");
-        do {
-            guess = (int) cin.read();
-            System.out.print(guess);
-        } while(guess >0 && guess<16);
-    }finally {
-        if (cin!= null) {
-            cin.close();
-        }
-    }
 }
 }
