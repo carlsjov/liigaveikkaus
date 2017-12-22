98211289b4c5d5c5a2cb472dd8dccb2674c6fdd8
JohanS <joalsj@utu.fi>
2017-12-23 01:47:55 +0200

    Teams.java poistettu

--- FileOut.java
+++ FileOut.java
@@ -5,8 +5,41 @@ import java.io.PrintWriter;
 import javax.imageio.IIOException;
 
 public class FileOut{
-
-    protected 
+    private FileWriter fw;
+    private PrintWriter pw;
+    
+    protected void FileOut (Teams[] teams, TeamGuess[] teamGuess){
+        Reader();
+        try{
+            pw.println("liiga");
+            for(int i=0;i<15;i++){
+                pw.println(teams[i].giveName());
+                pw.println(teams[i].givePlace());
+            }
+            if(teamGuess.lenght>0){
+                for(int k=0;t<teamGuess.lenght;k++){
+                    Teams[] teamsG = teamGuess[k].;
+                    for(int t=0;t<15;t++){
+                        pw.println(teamsG.;
+                        pw.println(teamsG.givePlace());
+                    }
+                }
+            }
+            pw.close();
+        }
+        catch (IOException e){
+           System.out.println("Ei toimikkaa...");
+        }
+    }
+    private void Reader(){
+        try{
+            fw = new FileWriter("testi.txt");
+            pw = new PrintWriter(fw);
+        }
+        catch (IOException e){
+            System.out.println("Tiedostoa ei l�ytynyt");
+        }
+    }
 
     public static void main(String[] args) {
      try{
--- TeamsGuess.java
+++ TeamsGuess.java
@@ -1,17 +1,21 @@
 /*
 TeamsGuess on arvaajan joukkue olio. Olio perii team olion ominaisuuksia Teams
 */
-public class TeamsGuess extends Teams{
+public class TeamsGuess{
+    final String plauerName;
+    final Teams[] teams;
     int points;
-    public TeamsGuess(String name,int place){
-        super(name);
-        this.place = place;
+    public TeamsGuess(String name,Teams[] teams){
+        plauerName = name;
+        this.teams = teams;
     }
     protected void setPoints(int points) {
         this.points = points;
     }
-    protected void setPlace(int place) {System.out.println("No posible");}
     protected int getPoints() {
         return points; 
+    }
+    protected Teams[] getTeams(){
+        return teams;
     }
 }
