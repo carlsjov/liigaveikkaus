c97305410372f5fc415ec69a115b808230260fb6
JohanS <joalsj@utu.fi>
2017-12-23 02:42:02 +0200

    Liiga tilanne tallentuu liiga.txt tiedostton ja arvaukset liigaArvaukset.txt tiedostoon

--- FileOut.java
+++ FileOut.java
@@ -7,41 +7,45 @@ import javax.imageio.IIOException;
 public class FileOut{
     private FileWriter fw;
     private PrintWriter pw;
+    private FileWriter fw2;
+    private PrintWriter pw2;
     
-    protected void FileOut (Teams[] teams){
+    public void FileOut (Teams[] teams){
         Reader();
         //try{
             for(int i=0;i<15;i++){
                 pw.println(teams[i].giveName());
                 pw.println(teams[i].givePlace());
             }
-            pw.println(0);
+            pw2.println(0);
+            pw2.close();
             pw.close();
         /*}
         catch (IOException e){
            System.out.println("Ei toimikkaa...");
         }*/
     }
-    
-    protected void FileOut (Teams[] teams, TeamsGuess[] teamGuess){
+
+    public void FileOut (Teams[] teams, TeamsGuess[] teamGuess){
         Reader();
         //try{
             for(int i=0;i<15;i++){
                 pw.println(teams[i].giveName());
                 pw.println(teams[i].givePlace());
             }
-            pw.println(teamGuess.length);
+            pw2.println(teamGuess.length);
             if(teamGuess.length>0){
                 for(int k=0;k<teamGuess.length;k++){
-                    pw.println(teamGuess[k].getName());
-                    pw.println(teamGuess[k].getPoints());
+                    pw2.println(teamGuess[k].getName());
+                    pw2.println(teamGuess[k].getPoints());
                     Teams[] teamsG = teamGuess[k].getTeams();
                     for(int t=0;t<15;t++){
-                        pw.println(teamsG[t].giveName());
-                        pw.println(teamsG[t].givePlace());
+                        pw2.println(teamsG[t].giveName());
+                        pw2.println(teamsG[t].givePlace());
                     }
                 }
             }
+            pw2.close();
             pw.close();
         /*}
         catch (IOException e){
@@ -52,6 +56,8 @@ public class FileOut{
         try{
             fw = new FileWriter("liiga.txt");
             pw = new PrintWriter(fw);
+            fw2 = new FileWriter("liigaArvaukset.txt");
+            pw2 = new PrintWriter(fw2);
         }
         catch (IOException e){
             System.out.println("Tiedostoa ei l�ytynyt");
@@ -60,14 +66,14 @@ public class FileOut{
 
     public static void main(String[] args) {
      try{
-         FileWriter fw = new FileWriter("testi.txt");
-         PrintWriter pw = new PrintWriter(fw);
+         FileWriter testif = new FileWriter("testi.txt");
+         PrintWriter testip = new PrintWriter(testif);
 
-         pw.println("moikka");
-         pw.println("moikka2");
+         testip.println("moikka");
+         testip.println("moikka2");
          //pw.println("moikka3");
          
-         pw.close();
+         testip.close();
      }
      catch (IOException e){
         System.out.println("Ei toimikkaa...");
